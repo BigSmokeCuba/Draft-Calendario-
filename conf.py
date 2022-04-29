42e38a02522a555423d178271a8860aa4189d647
BigSmokeCuba <101540930+BigSmokeCuba@users.noreply.github.com>
2022-04-29 03:23:49 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "cdbea1fb2a335600740eb67a465b3084"
-api_id = 12875919
-bot_token = "5317395920:AAFC22fvsvHUbje8rH9EHgQYFYgEbXGziZs"
-bot_admin_group = -1001607137860
+api_hash = "03bca249fdbe6824b1ca6d928a2e60ee"
+api_id = 13193029
+bot_token = "5246969712:AAF2e_-4vRcGhPNazPmIRugCzAkg_7FS5KI"
+bot_admin_group = -1001717170999
